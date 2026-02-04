2f5d7023a3f62edb22abf47db75af2cf48fc0dd3
mee-i <ilhamhizam9@gmail.com>
2026-02-04 10:10:39 +0700

    ref: change to bun sqlite

--- Web/db/index.ts
+++ Web/db/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
 
 export const db = drizzle(process.env.DB_FILE_NAME!);
 
